3c349a91ff33ac22da5767cc9fa6ef184bcbf55d
rjmccluskey <rjmccluskey@gmail.com>
2015-12-31 22:55:41 -0800

    Finish part 1 of "Tour of Heroes"

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,28 @@
 import {Component} from 'angular2/core';
 import 'rxjs/operator/map';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
-  selector: 'app',
-  moduleId: module.id,
-  template: `<h1>My First Angular 2 App</h1>`
+    selector: 'app',
+    moduleId: module.id,
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+        </div>
+    `
 })
-export class AppComponent {}
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
